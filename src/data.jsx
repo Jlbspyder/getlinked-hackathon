36e011a0caab9072180d088a382a248ab83b01f8
Debo Gbadebo <111382307+Jlbspyder@users.noreply.github.com>
2023-09-29 10:59:22 +0100

    Add files via upload

---
+++ src/data.jsx
@@ -0,0 +1,27 @@
+export const data = [
+  {
+    question: "Can I work on a project I started before the hackathon?",
+    answer:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, placeat nam doloremque iure eum atque.",
+  },
+  {
+    question: "What happens if I need help during the hackathon?",
+    answer:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, placeat nam doloremque iure eum atque.",
+  },
+  {
+    question: "What happens if I dont have an idea for a project?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus placeat expedita nisi ea consequatur ipsam architecto aperiam! Libero illum suscipit quos vel doloribus ex perferendis! Vitae quaerat labore error deserunt dicta expedita sunt at tenetur eos aliquid commodi asperiores numquam aut rerum, officia magnam voluptas cupiditate magni hic, nam veritatis!",
+  },
+  {
+    question: " Can I join a team or do I have to come with one?",
+    answer:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, placeat nam doloremque iure eum atque.",
+  },
+  {
+    question: "What happens after the hackathon ends?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta deserunt quidem vel, mollitia dicta nam deleniti, maiores velit optio excepturi assumenda itaque vero repellendus praesentium ea dolor dignissimos sint accusamus?",
+  },
+];
