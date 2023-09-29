d989b440ffc2707d6dd39962ca38d7ebeda639cf
Debo Gbadebo <111382307+Jlbspyder@users.noreply.github.com>
2023-09-29 11:02:35 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,101 @@ import HomePage from "./pages/HomePage";
 import Register from "./pages/Register";
 import Confirmation from "./pages/Confirmation";
 import Contact from "./pages/Contact";
+import MobileMenu from "./components/MobileMenu";
+import TimeLine from "./pages/TimeLine";
+import Faqs from "./pages/Faqs";
+import Overview from "./pages/Overview";
 
 function App() {
-  const  [openMenu, setOpenMenu] = useState(false)
+  const [openMenu, setOpenMenu] = useState(false);
 
   const handleOpen = () => {
-    setOpenMenu(true)
-  }
+    setOpenMenu(true);
+  };
   const handleClose = () => {
-    setOpenMenu(false)
-  }
+    setOpenMenu(false);
+  };
 
   return (
     <Router>
       <div className="container">
-        <Header handleOpen={handleOpen} open={openMenu} close={handleClose} />
+        <MobileMenu open={openMenu} close={handleClose} />
         <Routes>
-          <Route path="/" exact element={<HomePage handleClick={handleOpen} open={openMenu} close={handleClose}  />} />
-          <Route path="/register" element={<Register />} />
+          <Route
+            path="/"
+            exact
+            element={
+              <>
+                <Header
+                  handleOpen={handleOpen}
+                  open={openMenu}
+                  close={handleClose}
+                />
+                <HomePage
+                  handleClick={handleOpen}
+                  open={openMenu}
+                  close={handleClose}
+                />
+              </>
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <>
+                <Header
+                  handleOpen={handleOpen}
+                  open={openMenu}
+                  close={handleClose}
+                />
+                <Register />
+              </>
+            }
+          />
           <Route path="/confirmation" element={<Confirmation />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/overview" element={
+        <> 
+           <Header
+           handleOpen={handleOpen}
+           open={openMenu}
+           close={handleClose}
+         />
+          <Overview />
+        </>     
+          } />
+          <Route path="/faq" element={
+        <>
+          <Header
+          handleOpen={handleOpen}
+          open={openMenu}
+          close={handleClose}
+        />
+          <Faqs />
+        </>  
+          } />
+          <Route path="/timeline" element={
+        <>
+        <Header
+        handleOpen={handleOpen}
+        open={openMenu}
+        close={handleClose}
+        />
+          <TimeLine />
+        </>   
+          } />
+          <Route
+            path="/contact"
+            element={
+              <>
+                <Header
+                  handleOpen={handleOpen}
+                  open={openMenu}
+                  close={handleClose}
+                />
+                <Contact />
+              </>
+            }
+          />
         </Routes>
       </div>
     </Router>
