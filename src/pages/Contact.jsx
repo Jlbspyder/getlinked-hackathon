8f62be35714e07371dbb4a9def1d62cc520f1830
Debo Gbadebo <111382307+Jlbspyder@users.noreply.github.com>
2023-10-03 21:07:30 +0100

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FaAngleLeft } from "react-icons/fa";
 import { BsInstagram } from "react-icons/bs";
-import { BsTwitter } from "react-icons/bs";
+import { RiTwitterXFill } from "react-icons/ri";
 import { BsFacebook } from "react-icons/bs";
 import { BsLinkedin } from "react-icons/bs";
-import { BsPatchCheckFill } from "react-icons/bs"
+import { BsPatchCheckFill } from "react-icons/bs";
 
 const Contact = () => {
   const [errors, setErrors] = useState({});
@@ -69,81 +69,95 @@ const Contact = () => {
   return (
     <>
       <div className="call">
-        {submit && <div className="success">
+        {submit && (
+          <div className="success">
             <BsPatchCheckFill className="sent" />
             <p>Your message has been sent!</p>
-          </div>}
+          </div>
+        )}
         <img src="/images/sata-star.png" className="contact-satastar" />
         <img src="/images/sata-star.png" className="contact-satastar2" />
         <img src="/images/gray-star.png" className="contact-graystar" />
         <img src="/images/white-star.png" className="contact-whitestar" />
-        {!submit && <div className="call__info">
-          <Link to="/">
-            <button className="call close-btn">
-              <FaAngleLeft />
-            </button>
-          </Link>
-          <h2>
-            Questions or need assiatance?
-            <br />
-            Let us know about it
-          </h2>
-          <p>Email us below to any questions related to our event</p>
-        </div>}
-        {!submit && <form onSubmit={onSubmit}>
-          <div className="form-control">
-            <input
-              type="text"
-              name="team"
-              value={formData.team}
-              onChange={handleChange}
-            />
-            <label htmlFor="name">Team Name</label>
-            <small>{errors.team}</small>
+        {!submit && (
+          <div className="call__info">
+            <Link to="/">
+              <button className="call close-btn">
+                <FaAngleLeft />
+              </button>
+            </Link>
+            <h2>
+              Questions or need assistance?
+              <br />
+              Let us know about it
+            </h2>
+            <p>Email us below to any questions related to our event</p>
           </div>
-          <div className="form-control">
-            <input
-              type="text"
-              name="topic"
-              value={formData.topic}
-              onChange={handleChange}
-            />
-            <label htmlFor="name">Topic</label>
-            <small>{errors.topic}</small>
-          </div>
-          <div className="form-control">
-            <input
-              type="email"
-              name="mail"
-              value={formData.mail}
-              onChange={handleChange}
-            />
-            <label htmlFor="name">Email</label>
-            <small>{errors.mail}</small>
-          </div>
-          <div className="form-control">
-            <textarea
-              name="msg"
-              id="msg"
-              cols="30"
-              rows="15"
-              value={formData.msg}
-              onChange={handleChange}
-            />
-            <label htmlFor="name">Message</label>
-            <small>{errors.msg}</small>
-          </div>
-          <button className="mobile">Submit</button>
-        </form>}
+        )}
+        {!submit && (
+          <form onSubmit={onSubmit}>
+            <div className="form-control">
+              <input
+                type="text"
+                name="team"
+                value={formData.team}
+                onChange={handleChange}
+              />
+              <label htmlFor="name">Team Name</label>
+              <small>{errors.team}</small>
+            </div>
+            <div className="form-control">
+              <input
+                type="text"
+                name="topic"
+                value={formData.topic}
+                onChange={handleChange}
+              />
+              <label htmlFor="name">Topic</label>
+              <small>{errors.topic}</small>
+            </div>
+            <div className="form-control">
+              <input
+                type="email"
+                name="mail"
+                value={formData.mail}
+                onChange={handleChange}
+              />
+              <label htmlFor="name">Email</label>
+              <small>{errors.mail}</small>
+            </div>
+            <div className="form-control">
+              <textarea
+                name="msg"
+                id="msg"
+                cols="30"
+                rows="15"
+                value={formData.msg}
+                onChange={handleChange}
+              />
+              <label htmlFor="name">Message</label>
+              <small>{errors.msg}</small>
+            </div>
+            <button className="mobile">Submit</button>
+          </form>
+        )}
         <div className="post">
           <div className="share">
             <h3>Share on</h3>
           </div>
           <div className="call-socials">
-            <BsInstagram className="ig" />
-            <BsTwitter className="x" />
-            <BsFacebook className="fb" />
-            <BsLinkedin className="ind" />
+            <Link to="https://www.instagram.com/jlb_me/" target="_blank">
+              <BsInstagram className="ig" />
+            </Link>
+            <Link to="https://www.twitter.com/jlbspyder/" target="_blank">
+              <RiTwitterXFill className="x" />
+            </Link>
+            <Link to="https://www.facebook.com/" target="_blank">
+              <BsFacebook className="fb" />
+            </Link>
+            <Link to="https://www.pinterest.com/" target="_blank">
+              <BsLinkedin className="ind" />
+            </Link>
           </div>
         </div>
       </div>
@@ -177,70 +191,82 @@ const Contact = () => {
             </div>
             <div className="call-socials">
               <li>
-                <BsInstagram className="ig" />
+                <Link to="https://www.instagram.com/jlb_me/" target="_blank">
+                  <BsInstagram className="ig" />
+                </Link>
               </li>
               <li>
-                <BsTwitter className="x" />
+                <Link to="https://www.twitter.com/jlbspyder/" target="_blank">
+                  <RiTwitterXFill className="x" />
+                </Link>
               </li>
               <li>
-                <BsFacebook className="fb" />
+                <Link to="https://www.facebook.com/" target="_blank">
+                  <BsFacebook className="fb" />
+                </Link>
               </li>
               <li>
-                <BsLinkedin className="ind" />
+                <Link to="https://www.pinterest.com/" target="_blank">
+                  <BsLinkedin className="ind" />
+                </Link>
               </li>
             </div>
           </div>
         </div>
         <div className="form-wrapper">
-          {submit && <div className="success">
-            <BsPatchCheckFill className="sent" />
-            <p>Your message has been sent!</p>
-          </div>}
-          {!submit && <div className="form">
-            <h2>
-              Questions or need assiatance?
-              <br />
-              Let us know about it
-            </h2>
-            <form onSubmit={onSubmit}>
-              <div className="form-control">
-                <input
-                  type="text"
-                  name="team"
-                  value={formData.team}
-                  onChange={handleChange}
-                />
-                <label htmlFor="name">Team-Name</label>
-                <small>{errors.team}</small>
-              </div>
-              <div className="form-control">
-                <input
-                  type="text"
-                  name="mail"
-                  value={formData.mail}
-                  onChange={handleChange}
-                  className={errors.mail ? "error-input" : ""}
-                />
-                <label htmlFor="name">Email</label>
-                <small>{errors.mail}</small>
-              </div>
-              <div className="form-control">
-                <textarea
-                  name="msg"
-                  id="msg"
-                  cols="30"
-                  rows="10"
-                  value={formData.msg}
-                  onChange={handleChange}
-                />
-                <label htmlFor="name">Message</label>
-                <small>{errors.msg}</small>
-              </div>
-            </form>
-            <button className="btn sub" onClick={onSubmit}>
-              Submit
-            </button>
-          </div>}
+          {submit && (
+            <div className="success">
+              <BsPatchCheckFill className="sent" />
+              <p>Your message has been sent!</p>
+            </div>
+          )}
+          {!submit && (
+            <div className="form">
+              <h2>
+                Questions or need assiatance?
+                <br />
+                Let us know about it
+              </h2>
+              <form onSubmit={onSubmit}>
+                <div className="form-control">
+                  <input
+                    type="text"
+                    name="team"
+                    value={formData.team}
+                    onChange={handleChange}
+                  />
+                  <label htmlFor="name">Team-Name</label>
+                  <small>{errors.team}</small>
+                </div>
+                <div className="form-control">
+                  <input
+                    type="text"
+                    name="mail"
+                    value={formData.mail}
+                    onChange={handleChange}
+                    className={errors.mail ? "error-input" : ""}
+                  />
+                  <label htmlFor="name">Email</label>
+                  <small>{errors.mail}</small>
+                </div>
+                <div className="form-control">
+                  <textarea
+                    name="msg"
+                    id="msg"
+                    cols="30"
+                    rows="8"
+                    value={formData.msg}
+                    onChange={handleChange}
+                  />
+                  <label htmlFor="name">Message</label>
+                  <small>{errors.msg}</small>
+                </div>
+              </form>
+              <button className="btn sub" onClick={onSubmit}>
+                Submit
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </>
